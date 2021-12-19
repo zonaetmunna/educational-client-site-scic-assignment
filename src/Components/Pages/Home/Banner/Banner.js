c695fd80903771d2ec2d99bc586e8c30dcfd6363
zonaetmunna <zonaetmonna@gmail.com>
2021-12-19 18:44:14 +0600

    add

--- src/Components/Pages/Home/Banner/Banner.js
+++ src/Components/Pages/Home/Banner/Banner.js
@@ -5,8 +5,8 @@ const Banner = () => {
      return (
           <div>
                <Grid container spacing={2}>
-                    <Grid item xs={8}>
-                         <Typography variant="h4" gutterBottom component="div">
+                    <Grid item xs={12} md={6}>
+                         <Typography variant="h4" sx={{ color: 'info.main' }} gutterBottom component="div">
                               Access 2700+
                          </Typography>
                          <Typography variant="h4" gutterBottom component="div">
@@ -17,7 +17,7 @@ const Banner = () => {
                          </Typography>
                          <Button variant="contained">View all course</Button>
                     </Grid>
-                    <Grid item xs={4}>
+                    <Grid item xs={12} md={6}>
 
                     </Grid>
                </Grid>
